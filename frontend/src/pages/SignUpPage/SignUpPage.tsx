7b0bd572c2a08a6aaae80c42a00899cacde0c293
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-08 11:07:07 -0400

    feat: set up SignUpPage

--- frontend/src/pages/SignUpPage/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,7 +1,22 @@
 function SignUpPage() {
   return (
     <main>
-      <h1>This is the main sign up page</h1>
+      <h1>Sign up</h1>
+      <form>
+        <div>
+          <label htmlFor="name">Username</label>
+          <input id="username" name="username" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" />
+        </div>
+        <button type="submit">Create Account</button>
+      </form>
     </main>
   );
 }
