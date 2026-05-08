911f5200bc1323f4e2c64b24dde5c0a3868f6deb
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-08 13:43:10 -0400

    feat: check API health on frontend with fetch

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 function MainPage() {
+  const [apiStatus, setApiStatus] = useState("checking");
+
+  useEffect(() => {
+    async function checkApi() {
+      const response = await fetch("http://localhost:3000/api/health");
+      const data = await response.json();
+
+      setApiStatus(data.status);
+    }
+
+    checkApi();
+  });
   return (
     <main>
       <h1>This is the main page</h1>
+      <p>API status: {apiStatus}</p>
     </main>
   );
 }
