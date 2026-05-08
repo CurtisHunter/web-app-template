c1ed58552cc70cbe019c61b85caaaef75d93e0c3
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-08 13:45:15 -0400

    feat: use cors in backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use("/", indexRouter);
 
