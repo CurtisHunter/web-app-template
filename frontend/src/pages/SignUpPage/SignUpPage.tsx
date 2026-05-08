387f4333d830c0e9db95e06daacfd2dea6607571
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-08 11:23:39 -0400

    feat: sign up page controlled components & submit

--- frontend/src/pages/SignUpPage/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,19 +1,53 @@
+import { useState } from "react";
+
 function SignUpPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log({
+      name,
+      email,
+      password,
+    });
+  }
+
   return (
     <main>
       <h1>Sign up</h1>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div>
           <label htmlFor="name">Name</label>
-          <input id="name" name="name" type="text" />
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input id="password" name="password" type="password" />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
         <button type="submit">Create Account</button>
       </form>
