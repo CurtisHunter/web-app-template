7506214e9498411bcec826ef792c5051a895391e
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-09 16:25:19 -0400

    feat(db): add users table initialisation

--- backend/db/init.js
+++ backend/db/init.js
@@ -1,6 +1,23 @@
 require("dotenv").config();
 const pool = require("./pool");
 
-async function initializeDatabase() {}
+async function initializeDatabase() {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            name VARCHAR(30) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL,
+            created_at TIMESTAMPTZ DEFAULT NOW()
+        );
+    `);
+  console.log("Database initialized");
+}
 
-initializeDatabase();
+initializeDatabase()
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    pool.end();
+  });
