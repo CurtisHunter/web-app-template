9e137c658aca5a45e11f715a391fee34e8a95cea
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-09 15:23:56 -0400

    feat: validate sign up post requests

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,41 @@
-exports.signUp = async (req, res) => {
-  res.json({ message: "sign up route", recieved: req.body });
-};
+const { body, validationResult, matchedData } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "Must be between 1 and 30 characters";
+const emailErr = "Must be an email";
+const passwordErr = "must be at least 8 characters";
+
+const validateSignUp = [
+  body("name")
+    .trim()
+    .matches(/^[A-Za-z ]+$/)
+    .withMessage(`Name ${alphaErr}`)
+    .isLength({ min: 1, max: 30 })
+    .withMessage(`First name ${lengthErr}`),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage(`email ${emailErr}`)
+    .normalizeEmail(),
+  body("password").isLength({ min: 8 }).withMessage(`Password ${passwordErr}`),
+];
+
+exports.signUp = [
+  validateSignUp,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    const { name, email, password } = matchedData(req);
+    res.json({ message: "sign up route", received: { name, email, password } });
+  },
+];
 
 exports.signIn = async (req, res) => {
-  res.json({ message: "sign in route", recieved: req.body });
+  res.json({ message: "sign in route", received: req.body });
 };
 
 exports.signOut = async (req, res) => {
