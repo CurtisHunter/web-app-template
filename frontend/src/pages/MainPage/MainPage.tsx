7de0a7eda4cff95ff2000fc1c4dd0959c01d3b50
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-12 15:09:17 -0400

    feat: handle sign out on frontend

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -6,6 +6,24 @@ function MainPage() {
 
   const navigate = useNavigate();
 
+  async function handleSignOut() {
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/sign-out", {
+        method: "POST",
+        credentials: "include",
+      });
+
+      // Check if the request was successful
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      navigate("/users/sign-in");
+    } catch (error) {
+      console.error("Error posting data (sign out):", error);
+    }
+  }
+
   useEffect(() => {
     async function checkAuth() {
       const response = await fetch("http://localhost:3000/api/auth/me", {
@@ -26,6 +44,9 @@ function MainPage() {
   }, [navigate]);
   return (
     <main>
+      <button type="button" onClick={handleSignOut}>
+        Sign out
+      </button>
       <h1>This is the main page</h1>
       <p>Signed in as: {userName}</p>
     </main>
