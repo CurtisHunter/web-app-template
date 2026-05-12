8c83081a287b9b0f34bfd5b800ae0c187658e6a7
Curtis Hunter <95283125+CurtisHunter@users.noreply.github.com>
2026-05-12 15:22:43 -0400

    feat(auth): navigate after sign up

--- frontend/src/pages/SignUpPage/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 function SignUpPage() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -23,6 +25,11 @@ function SignUpPage() {
     });
 
     const data = await response.json();
+
+    if (response.ok) {
+      navigate("/users/sign-in");
+      return;
+    }
 
     console.log(data);
   }
